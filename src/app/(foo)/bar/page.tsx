d990174d258e5779d949082f3cf0672a716b18a8
米田宏 <yonedahiroshi1229@gmail.com>
2025-03-29 07:58:42 +0900

    foo

--- src/app/(foo)/bar/page.tsx
+++ src/app/(foo)/bar/page.tsx
@@ -1,6 +1,8 @@
+import styles from './page.module.css';
+
 export default function Page() {
     return <>
-        <h2>bar</h2>
+        <h2 className={styles.foo}>bar</h2>
         <p>urlは、http://localhost:3000/bar</p>
     </>;
 }
