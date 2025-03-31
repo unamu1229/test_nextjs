c78b4a8100c50802efa65bf53d576662e5a37de6
米田宏 <yonedahiroshi1229@gmail.com>
2025-03-31 15:46:22 +0900

    演習4-3

--- src/app/recruit/page.tsx
+++ src/app/recruit/page.tsx
@@ -1,11 +1,17 @@
 'use client'
 
-import {useState} from "react";
+import {ChangeEvent, ChangeEventHandler, KeyboardEventHandler, MouseEventHandler, useState} from "react";
 import { TodoItem } from "./TodoItem";
 
 export default function Page() {
 
-    const todoListInit = [
+    type Todo = {
+        id: number;
+        task: string;
+        completed: boolean;
+    }
+
+    const todoListInit: Todo[] = [
         { id: 1, task: "Learning Browser", completed: true },
         { id: 2, task: "Learning JavaScript/TypeScript", completed: true },
         { id: 3, task: "Learning React", completed: false },
@@ -34,8 +40,39 @@ export default function Page() {
         changeTodoList(changedTodoList);
     }
 
+    const [newTodo, setNewTodo] = useState("");
+    const changeNewTodo: ChangeEventHandler<HTMLInputElement> = (event: ChangeEvent<HTMLInputElement>) => {
+        setNewTodo(event.currentTarget.value);
+    };
+
+    const addTodoFromEnter: KeyboardEventHandler<HTMLInputElement> = (event) => {
+        if (event.key !== "Enter") {
+            return;
+        }
+        addTodo();
+    }
+
+    const addTodoFromButton: MouseEventHandler<HTMLButtonElement> = (event) => {
+        addTodo();
+    }
+
+    function addTodo() {
+        if (newTodo === "") {
+            return;
+        }
+
+        const currentMaxId: number = todoList.reduce((accumulator:number , todo: Todo) => {
+            return accumulator > todo.id ? accumulator : todo.id;
+        }, 0);
+        const newTodoList = [...todoList, {id: currentMaxId + 1, task: newTodo, completed: false}];
+        changeTodoList(newTodoList);
+        setNewTodo("");
+    }
+
+
     return <>
         <h1>RecruitのReact研修</h1>
+        <input type="text" value={newTodo} onChange={changeNewTodo} onKeyDown={addTodoFromEnter}/> <button onClick={addTodoFromButton}>追加</button>
         <ul>
             {
                 todoList.map((todo) => {
